789156e7393ead479c4285976470354b8bd2d584
Andrew Esteves <esteves.andrew@gmail.com>
2021-03-13 11:25:08 +0000

    Lang PT BR added

---
+++ resources/lang/pt_BR/messages.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages
+    |--------------------------------------------------------------------------
+    |
+    | General application messages
+    |
+    */
+
+    'register_success' => 'Registrado com sucesso',
+    'register_failed' => 'O registro falhou',
+    'register_greeting' => 'Oi',
+    'register_thanks' => 'Obrigado por se inscrever em',
+    'register_confirm' => 'Para começar, clique no link abaixo para verificar se temos seu endereço de e-mail correto.',
+    'general_update' => 'Operação de atualização realizada com sucesso',
+    'general_error' => 'Ups, ocorreu um erro, tente novamente',
+    'general_create' => 'Operação de criação realizada com sucesso',
+    'general_destroy' => 'Operação de remoção realizada com sucesso',
+    'general_success' => 'Operação realizada com sucesso',
+    'queue_enroll_many' => 'Estamos importando seus alunos, estaremos de volta em breve',
+
+];
